b845f9a1c82f1bb45825979bb3637bc7e4aed53a
Liutauras <lidi.uk@gmail.com>
2015-10-28 15:33:58 +0100

    Add movie entity. Add The Movie DB API interface

--- app/src/main/java/pm/diu/liutauras/udacitypopularmovies/model/rest/TheMovieDBApi.java
+++ app/src/main/java/pm/diu/liutauras/udacitypopularmovies/model/rest/TheMovieDBApi.java
@@ -1,4 +1,14 @@
 package pm.diu.liutauras.udacitypopularmovies.model.rest;
 
-public class TheMovieDBApi {
+import java.util.List;
+import pm.diu.liutauras.udacitypopularmovies.model.entities.Movie;
+import retrofit.http.GET;
+import retrofit.http.Query;
+import rx.Observable;
+
+public interface TheMovieDBApi {
+
+  @GET("3/discover/movie")
+  Observable<List<Movie>> loadMovies(@Query("api_key") String api_key);
+
 }
